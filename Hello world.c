1c258179e9785be7f051673b302e198f7785cbb3
Aljas012 <115525838+Aljas012@users.noreply.github.com>
2022-10-11 17:32:25 +0800

    Add files via upload

---
+++ Hello world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   int numb = 12;
+   char letter = 'G';
+
+   printf("My favorite number is %d\n",numb);
+   printf("My favorite letter is %c", letter);
+
+   return 0;
+}
